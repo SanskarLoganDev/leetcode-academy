06f3e98bd6e2e6ea55bbed0da21604906d90b96e
SanskarLoganDev <devlogan70@gmail.com>
2024-10-02 11:52:23 -0400

    Problem to understand usage of stacks

---
+++ easy/valid_paranthesis.py
@@ -0,0 +1,19 @@
+def isValid(s):
+    flag = False
+    bracket_relation = {'(':')','[':']','{':'}'}
+    bracket_list = [] # stack
+    for bracket in s:
+        if bracket in bracket_relation.keys():
+            bracket_list.append(bracket)
+        elif bracket in bracket_relation.values():
+            if not bracket_list:
+                return False # checking if closing bracket found in string when stack is empty
+            if bracket!= bracket_relation[bracket_list[-1]]:
+                return False
+            bracket_list.pop()
+    if len(bracket_list)==0:
+        return True
+    else:
+        return False
+
+print(isValid("()}[]"))
