b9c6967412516772564ac2b86b7375ef6bd215df
SanskarLoganDev <devlogan70@gmail.com>
2025-02-19 11:42:45 -0500

    Merge strings alternatively, another solution

--- easy/String/merge_strings_alternatively.py
+++ easy/String/merge_strings_alternatively.py
@@ -43,3 +43,16 @@ def mergeAlternatively(word1, word2):
     return res
 
 print(mergeAlternatively(word1 = "abcd", word2 = "pq"))
+
+def mergeAlternatively2(word1, word2):
+    result=[]
+    i=0
+    while i < len(word1) or i < len(word2):
+        if i < len(word1):
+            result.append(word1[i])
+        if i < len(word2):
+            result.append(word2[i])
+        i+=1
+    return ''.join(result)
+
+print(mergeAlternatively2(word1 = "abcd", word2 = "pq"))
