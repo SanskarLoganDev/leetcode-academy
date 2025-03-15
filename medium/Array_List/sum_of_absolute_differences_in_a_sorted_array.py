e5eecd18c8f4669e4a4a06e5b379682d22b1b852
SanskarLoganDev <devlogan70@gmail.com>
2025-03-15 10:59:23 -0400

    Solution of sum of absolute differences

---
+++ medium/Array_List/sum_of_absolute_differences_in_a_sorted_array.py
@@ -0,0 +1,39 @@
+# 1685. Sum of Absolute Differences in a Sorted Array
+
+# You are given an integer array nums sorted in non-decreasing order.
+
+# Build and return an integer array result with the same length as nums such that result[i] is equal to the summation of absolute differences between nums[i] and all the other elements in the array.
+# In other words, result[i] is equal to sum(|nums[i]-nums[j]|) where 0 <= j < nums.length and j != i (0-indexed).
+
+
+# Example 1:
+
+# Input: nums = [2,3,5]
+# Output: [4,3,5]
+# Explanation: Assuming the arrays are 0-indexed, then
+# result[0] = |2-2| + |2-3| + |2-5| = 0 + 1 + 3 = 4,
+# result[1] = |3-2| + |3-3| + |3-5| = 1 + 0 + 2 = 3,
+# result[2] = |5-2| + |5-3| + |5-5| = 3 + 2 + 0 = 5.
+# Example 2:
+
+# Input: nums = [1,4,6,8,10]
+# Output: [24,15,13,15,21]
+
+class Solution(object):
+    def getSumAbsoluteDifferences(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        res = []
+        for i in range(0,len(nums)):
+            sum = 0
+            for j in range(0, len(nums)):
+                sum+=abs(nums[i]-nums[j])
+            res.append(sum)
+        return res
+        
+nums = [2,3,5]
+res = Solution()
+ans = res.getSumAbsoluteDifferences(nums)
+print(ans)
