2f570d1e1c4221e25376d92810fd188063481a23
SanskarLoganDev <devlogan70@gmail.com>
2025-06-18 23:11:37 -0400

    3sum solu

--- medium/Array_List/3sum.py
+++ medium/Array_List/3sum.py
@@ -1,5 +1,5 @@
 # 15. 3Sum (Neetcode 150) Important
-
+# Topics: Array, Two Pointers, Sorting
 # Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
 
 # Notice that the solution set must not contain duplicate triplets.
