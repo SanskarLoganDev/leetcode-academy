4388765e7d217afcf44d75efc98ac878e3e0a0f7
SanskarLoganDev <devlogan70@gmail.com>
2025-06-20 17:11:11 -0400

    3. Longest Substring Without Repeating Characters (Neetcode 150) additional comments

--- medium/String/longest_substring_without_repeating_chars.py
+++ medium/String/longest_substring_without_repeating_chars.py
@@ -1,7 +1,7 @@
-# 3. Longest Substring Without Repeating Characters Important
+# 3. Longest Substring Without Repeating Characters (Neetcode 150) Important
 
 # Given a string s, find the length of the longest substring without duplicate characters.
-
+# topics: Hash Table, Two Pointers, String
 
 # Example 1:
 
@@ -20,6 +20,11 @@
 # Explanation: The answer is "wke", with the length of 3.
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
 
+# Constraints:
+
+# 0 <= s.length <= 5 * 104
+# s consists of English letters, digits, symbols and spaces.
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         char_index = {} # dictionary {"element": "its index in s"}
@@ -28,7 +33,7 @@ class Solution:
         
         for i in range(len(s)):
             # If the character is already seen and is within the current window:
-            if s[i] in char_index and char_index[s[i]] >= start:
+            if s[i] in char_index and char_index[s[i]] >= start: # here we have char_index[s[i]] >= start, to check if the character is within the current window otherwise the value of start will go backwards
                 # Move the start to one position after the last occurrence of ch.
                 start = char_index[s[i]] + 1
             # Update the last seen index for ch.
