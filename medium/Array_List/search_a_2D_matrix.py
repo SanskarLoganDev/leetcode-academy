cfe04758303faef4dfc4d0f05191ef3fc50fb39d
SanskarLoganDev <devlogan70@gmail.com>
2025-07-04 18:06:00 -0400

    74. Search a 2D Matrix optimised solution

--- medium/Array_List/search_a_2D_matrix.py
+++ medium/Array_List/search_a_2D_matrix.py
@@ -1,4 +1,4 @@
-# 74. Search a 2D Matrix
+# 74. Search a 2D Matrix (NEETCODE 150) Important
 
 # You are given an m x n integer matrix matrix with the following two properties:
 
@@ -43,4 +43,34 @@ class Solution:
                     r = mid-1
                 else:
                     l = mid+1
+        return False
+    
+    
+ # Time complexity O(log(m * n)) due to binary search to find the the target row and the target element in that row, space complexity O(1)   
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+
+        top = 0
+        bot = len(matrix)-1
+        while top<=bot:
+            mid_row = (top+bot)//2
+            if target>matrix[mid_row][-1]: #If target is greater than the last element of the mid_row, then we need to search in the lower rows
+                top = mid_row+1
+            elif target<matrix[mid_row][0]: #If target is less than the first element of the mid_row, then we need to search in the upper rows
+                bot = mid_row-1
+            else:
+                break
+        if not top<=bot:
+            return False
+
+        target_row = matrix[(top+bot)//2] # This is the row where the target could be present, since it is between the first and last elements of this row
+        l, r= 0, len(matrix[0])-1
+        while l<=r:
+            mid = (l+r)//2
+            if target_row[mid]==target:
+                return True
+            elif target<target_row[mid]:
+                r = mid-1
+            else:
+                l = mid+1
         return False
