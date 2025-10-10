2743b45949db23e38767ebc4d2976b015e264583
SanskarLoganDev <devlogan70@gmail.com>
2025-10-09 20:56:53 -0400

    73. Set Matrix Zeroes optimised solution

--- medium/Math/set_matrix_zeroes.py
+++ medium/Math/set_matrix_zeroes.py
@@ -40,18 +40,64 @@ class Solution:
         """
         m = len(matrix)
         n = len(matrix[0])
-        cellsi = []
-        cellsj = []
+        cellsi = set()
+        cellsj = set()
         for i in range(0,m):
             for j in range(0, n):
                 if matrix[i][j]==0:
-                    cellsi.append(i)
-                    cellsj.append(j)
+                    cellsi.add(i) # add the row index to the set
+                    cellsj.add(j) # add the column index to the set
 
         for i in range(0,m):
             for j in range(0, n):
                 if i in cellsi or j in cellsj:
                     matrix[i][j]=0
         
+# time complexity: O(M*N) where M is the number of rows and N is the number of columns in the matrix
+# space complexity: O(1)
 
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        firstRowImpacted = False
+        firstColImpacted = False
+        # Step 1
+        # check if first row is impacted
+        for col in range(n):
+            if matrix[0][col]==0:
+                firstRowImpacted = True
+                break
+
+        # check if first col is impacted
+        for row in range(m):
+            if matrix[row][0]==0:
+                firstColImpacted = True
+                break
+
+        # Step 2
+        # Set markers in first row and column
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][j]==0:
+                    matrix[i][0]=0
+                    matrix[0][j]=0
         
+        # Step 3: setting matrix values to zero if their starting values are zero
+        for i in range(1, m):
+            for j in range(1, n):
+                if matrix[i][0]==0 or matrix[0][j]==0:
+                    matrix[i][j]=0
+
+        # Step 4: Handling the first row and column
+        if firstRowImpacted:
+            for col in range(n):
+                matrix[0][col]=0
+        
+        if firstColImpacted:
+            for row in range(m):
+                matrix[row][0]=0
+
