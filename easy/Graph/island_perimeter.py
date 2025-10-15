5bc6ca10e2517a4becadb8e239a16545fe6f3a23
SanskarLoganDev <devlogan70@gmail.com>
2025-10-15 15:09:35 -0400

    463. Island Perimeter BFS Solution

--- easy/Graph/island_perimeter.py
+++ easy/Graph/island_perimeter.py
@@ -5,7 +5,6 @@
 # Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
 
 # The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
- 
 
 # Example 1:
 
@@ -66,3 +65,43 @@ class Solution:
                 if grid[i][j]==1:
                     dfs(grid, i, j) # call the dfs function
                     return self.perimeter
+
+# Solution using BFS:
+
+# time complexity O(M*N)
+# Space complexity: O(M*N) for queue data structure
+
+from collections import deque
+class Solution:
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m = len(grid) # length of column
+        n = len(grid[0]) # length of row
+        perimeter = 0
+        q = deque()
+        directions = [[1,0], [-1,0], [0,1],[0,-1]] # to traverse in 4 directions
+        for i in range(m):
+            if q:
+                break
+            for j in range(n):
+                if grid[i][j]==1:
+                    q.append((i,j))
+                    grid[i][j]=-1
+                    break
+        if not q: # if there is no land (edge case)
+            return 0
+        while q:
+            i, j = q.popleft()
+            # check the 4 neighbours: (i+1, j), (i-1, j), (i, j+1), (i, j-1), defined in directions array
+
+            for d in directions:
+                ni = i + d[0] # here we did not do i = i + d[0] because we need the original i, j values for other directions
+                nj = j + d[1]
+                if ni<0 or nj<0 or ni>=m or nj>=n or grid[ni][nj]==0:
+                    perimeter+=1 # if it is out of bound or it is water, we increase the perimeter by 1
+                    continue
+                if grid[ni][nj]==-1: # already visited
+                    continue
+                else:
+                    q.append((ni, nj)) # add to queue
+                    grid[ni][nj]=-1 # mark visited
+        return perimeter
