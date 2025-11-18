a20f41f4c9139ca39f5a098cb25fbcf68574c405
SanskarLoganDev <devlogan70@gmail.com>
2025-11-17 23:28:35 -0500

    1791. Find Center of Star Graph added comments

--- easy/Graph/center_of_star_graph.py
+++ easy/Graph/center_of_star_graph.py
@@ -32,9 +32,10 @@ from typing import List
 
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        n = len(edges)+1
-        indegree = [0]*(n+1)
-        outdegree = [0]*(n+1)
+        n = len(edges)+1 # n is number of nodes
+        indegree = [0]*(n+1) # since nodes are 1 indexed to n. Indegree is number of edges coming into a node
+        outdegree = [0]*(n+1) # outdegree is number of edges going out of a node
+         # in a star graph, the center node will have indegree + outdegree = n-1 as it is connected to every other node
         for a, b in edges:
             outdegree[a]+=1
             indegree[b]+=1
@@ -47,6 +48,6 @@ class Solution:
 # the centre has to be a part of every pair, so we just compare the 1st and second pair and return the common element
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        if edges[0][0] in edges[1]:
-            return edges[0][0]
+        if edges[0][0] in edges[1]: # check if first element of first pair is in second pair
+            return edges[0][0] 
         return edges[0][1]
