ecb6362431eba06a9ac55b580136b17aee4e1563
SanskarLoganDev <devlogan70@gmail.com>
2025-12-03 18:16:35 -0500

    547. Number of Provinces solution using the concept of DFS and BFS to traverse the undirected graph.

--- medium/Graphs/number_of_provinces.py
+++ medium/Graphs/number_of_provinces.py
@@ -30,6 +30,8 @@
 # time complexity: O(N^2)
 # space complexity: O(N^2) for adjacency list
 
+# Using DFS
+# Using the concept of DFS to traverse the undirected graph
 from typing import List
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
@@ -43,19 +45,54 @@ class Solution:
                 if isConnected[i][j]==1:
                     adj[i].append(j)
 
-        def DFS(adj, u, visited, parent):
+        def DFS(adj, u, visited): # we don't need parent here as the graph is undirected but we are using visited array to avoid cycles
             visited[u]=True
             for v in adj[u]:
-                if v == parent: # we don't want to go back to parent
-                    continue
                 if visited[v]==True:
                     continue # we continue instead of returning because we want to visit all nodes in this component
-                DFS(adj, v, visited, u)
+                DFS(adj, v, visited)
             return
         count = 0
         for u in range(n):
             if visited[u]!=True:
-                DFS(adj, u, visited, -1) # we pass -1 as parent of starting node
+                DFS(adj, u, visited)
                 count+=1 # we increase count when we start a new DFS, meaning we found a new province
+        return count
+    
+# Using BFS
+# Using the concept of BFS to traverse the undirected graph
+from collections import deque
+
+# time complexity: O(N^2)
+# space complexity: O(N^2) for adjacency list
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        visited = [False]*n
+        adj = [[] for _ in range(n)]
+        for i in range(n):
+            for j in range(n):
+                if i==j:
+                    continue
+                if isConnected[i][j]==1:
+                    adj[i].append(j)
+
+        def BFS(adj, u, visited):
+            q = deque()
+            q.append(u) # enqueue
+            visited[u] = True
+            while q:
+                source = q.popleft()
+                for v in adj[source]:
+                    if visited[v]==True:
+                        continue
+                    q.append(v) # enqueue the unvisited node
+                    visited[v] = True # mark as visited when enqueued to avoid multiple enqueueing
+            return
+        count = 0
+        for u in range(n):
+            if visited[u]!=True:
+                BFS(adj, u, visited) # start BFS from unvisited node
+                count+=1
         
         return count
