8935c6b3ea88caadcbd478fa193a899fe40fdc3a
SanskarLoganDev <devlogan70@gmail.com>
2026-01-10 08:14:11 +0530

    212. Word Search II solution

---
+++ hard/word_search2.py
@@ -0,0 +1,72 @@
+# 212. Word Search II
+# Neetcode 150 (Important)
+
+# Given an m x n board of characters and a list of strings words, return all words on the board.
+
+# Each word must be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once in a word.
+
+# Example 1:
+# https://assets.leetcode.com/uploads/2020/11/07/search1.jpg
+# Input: board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]
+# Output: ["eat","oath"]
+
+# Example 2:
+# https://assets.leetcode.com/uploads/2020/11/07/search2.jpg
+# Input: board = [["a","b"],["c","d"]], words = ["abcb"]
+# Output: []
+ 
+# Constraints:
+
+# m == board.length
+# n == board[i].length
+# 1 <= m, n <= 12
+# board[i][j] is a lowercase English letter.
+# 1 <= words.length <= 3 * 104
+# 1 <= words[i].length <= 10
+# words[i] consists of lowercase English letters.
+# All the strings of words are unique.
+
+# Solution uses Backtracking but does not use trie
+# Time complexity O(M * N * 3^L * W) where M,N are dimensions of board, L is length of longest word, W is number of words
+# From a cell you can try up to 4 neighbors, but once you move, the previous cell is marked '#', so you cannot immediately go back.
+# That makes the branching roughly 4 choices for the first step, then at most 3 choices thereafter
+
+# Space complexity O(L) where L is length of longest word (recursion stack)
+# res set stores up to W words -> O(W) (output-dependent).
+from typing import List
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        m = len(board)
+        n = len(board[0])
+        directions = [(0,1), (0,-1), (1,0), (-1, 0)]
+        def backtrack(i, j, level, word): # here word is the current word we are searching for
+            if level==len(word): # all letters matched
+                return True
+            if i<0 or j<0 or i>=m or j>=n: # out of bounds
+                return False
+            
+            if board[i][j]=="#": # already visited
+                return False
+
+            if board[i][j]!=word[level]: # current letter does not match
+                return False
+
+            temp = board[i][j] # store the current letter
+            board[i][j] = '#' # mark as visited
+            for d in directions:
+                ni = i+d[0]
+                nj = j+d[1]
+                if backtrack(ni, nj, level+1, word): # move to next letter
+                    board[i][j] = temp # restore the letter before returning
+                    return True
+            board[i][j] = temp # restore the letter before backtracking
+            return False
+        res = set() # use set to avoid duplicates
+        for word in words: # for each word in words list
+            for i in range(m): # for each row
+                for j in range(n): # for each column
+                    if word[0]==board[i][j]: # if first letter matches
+                        if backtrack(i, j, 0, word): # start backtracking
+                            res.add(word)
+
+        return list(res) # convert set to list
