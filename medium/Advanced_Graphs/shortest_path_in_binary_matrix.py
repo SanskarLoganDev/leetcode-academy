7b952fb4c7a3700a7788e97ca715185e8aa992d5
SanskarLoganDev <devlogan70@gmail.com>
2026-01-14 05:39:27 +0530

    1091. Shortest Path in Binary Matrix solution using dijkstra's algorithm

--- medium/Advanced_Graphs/shortest_path_in_binary_matrix.py
+++ medium/Advanced_Graphs/shortest_path_in_binary_matrix.py
@@ -78,3 +78,37 @@ class Solution:
 
         return val
 
+# Dijkstra's Algorithm Approach with min-heap (could also be done using queue as all edge weights are same)
+# time complexity O(n^2) where n is the number of rows or columns in the grid
+# space complexity O(n^2) due to result array and min-heap usage
+import heapq
+class Solution:
+    def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        directions = [(0,1), (1,0), (-1,0), (0,-1), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+        if grid[0][0]!=0 or grid[n-1][n-1]!=0:
+            return -1
+        if len(grid)==1:
+            return 1
+        result = [[float("inf")]*n for _ in range(n)]
+        min_heap = []
+        result[0][0] = 0
+        heapq.heappush(min_heap, (0,0,0)) # dist, i, j
+        while min_heap:
+            dist, i, j = heapq.heappop(min_heap)
+            if i==n-1 and j==n-1:
+                return dist+1 # we do +1 here because we are returning upon reaching the destination cell and the destination cell needs to be counted in the path length
+            for d in directions:
+                ni = i+d[0]
+                nj = j+d[1]
+                if ni<0 or nj<0 or ni>=n or nj>=n:
+                    continue
+
+                if grid[ni][nj]!=0:
+                    continue
+                grid[ni][nj]=1 # mark visited. Not much use here as we are using result array to track distances and repeated visits to a cell will only increse the distance and thus won't be considered anyways
+                if dist+1< result[ni][nj]: # we do +1 here because each edge length is same = 1
+                    heapq.heappush(min_heap, (dist+1, ni, nj))
+                    result[ni][nj] = dist+1
+
+        return -1 # if we exhaust all options and don't reach the end
