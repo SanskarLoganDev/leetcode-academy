096be92b29c2485d055650773d17d8a1440dfb31
SanskarLoganDev <devlogan70@gmail.com>
2026-01-20 17:20:06 +0530

    746. Min Cost Climbing Stairs added comments

--- easy/Dynamic Programming/min_cost_climbing_stairs.py
+++ easy/Dynamic Programming/min_cost_climbing_stairs.py
@@ -290,9 +290,9 @@ class Solution:
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
         n=len(cost)
-        dp = [0]*n
-        dp[0]=cost[0]
-        dp[1]=cost[1]
+        dp = [0]*n # this dp array stores the minimum cost to reach each step
+        dp[0]=cost[0] # cost to reach step 0 is cost[0]
+        dp[1]=cost[1] # cost to reach step 1 is cost[1]
         for i in range(2,n):
             dp[i] = cost[i]+min(dp[i-1], dp[i-2])
         return min(dp[n-1],dp[n-2])
