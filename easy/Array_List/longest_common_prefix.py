e1c5423424145b8be2ad5ccd276c6098ce1b151c
SanskarLoganDev <devlogan70@gmail.com>
2026-03-15 11:30:06 -0400

    14. Longest Common Prefix solution

--- easy/Array_List/longest_common_prefix.py
+++ easy/Array_List/longest_common_prefix.py
@@ -1,37 +1,54 @@
-# 14 LONGEST COMMON PREFIX
+# 14. Longest Common Prefix
+# Neetcode 250
 
 # Write a function to find the longest common prefix string amongst an array of strings.
 
 # If there is no common prefix, return an empty string "".
 
- 
-
 # Example 1:
 
 # Input: strs = ["flower","flow","flight"]
 # Output: "fl"
+
 # Example 2:
 
 # Input: strs = ["dog","racecar","car"]
 # Output: ""
 # Explanation: There is no common prefix among the input strings.
 
+# Constraints:
 
+# 1 <= strs.length <= 200
+# 0 <= strs[i].length <= 200
+# strs[i] consists of only lowercase English letters if it is non-empty.
 
-def longestCommonPrefix(strs) -> str:
-    prefix = ""
-    strs.sort()
-    first = strs[0]
-    last = strs[len(strs)-1]
-    print(first, last)
-    min_len = min(len(first),len(last))
-    print(min_len)
-    for i in range(min_len):
-        if first[i]!=last[i]:
-            break
-        else:
-            prefix=prefix+first[i]    
-        
-    return prefix
-print(longestCommonPrefix(["dog","racecar","car"]))
+# Simple logic: if we sort them then the most difference will be in the first and last elements as sort function sorts lexicographically
+# time complexity: O(nlogn)+O(L) whre L is common subtring length
+# space complexity: O(L)
+from typing import List
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        common = ""
+        strs.sort()
+        first = strs[0]
+        last = strs[len(strs)-1]
+        for i in range(min(len(first), len(last))):
+            if first[i]!=last[i]:
+                break
+            common+=first[i]
+        return common
+
+# using the same concept but using min max instead as they also evaluate lexicographically
+# time complexity: O(n.L)
+# space complexity: O(L)
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        common = ""
+        first = min(strs)
+        last = max(strs)
+        for i in range(min(len(first), len(last))):
+            if first[i]!=last[i]:
+                break
+            common+=first[i]
+        return common
 
