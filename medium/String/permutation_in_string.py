6f1a20d8c87a545d17b060ef94a7ef530c0c9b6b
SanskarLoganDev <devlogan70@gmail.com>
2026-07-25 15:24:38 -0400

    567. Permutation in String  added brute force solution and optimized solution with sliding window technique

--- medium/String/permutation_in_string.py
+++ medium/String/permutation_in_string.py
@@ -1,4 +1,5 @@
-# 567. Permutation in String (Neetcode 150) Important
+# 567. Permutation in String 
+# (Neetcode 150) Important
 
 # Given two strings s1 and s2, return true if s2 contains a permutation of s1, or false otherwise.
 
@@ -19,47 +20,45 @@
 # 1 <= s1.length, s2.length <= 104
 # s1 and s2 consist of lowercase English letters.
 
+# Brute force solution
+# Time complexity: O(N.MlogM), Space complexity: O(M)
 
-# My inital solution with time complexity O(N+M) where N is the length of s1 and M is the length of s2. 
-# The 2nd solution is more optimized.
 class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1)>len(s2):
+    def permutationStr(self, s1: str, s2: str) -> bool:
+        m = len(s1)
+        n = len(s2)
+        if m>n:
             return False
-        count1 = [0]*26
-        count2 = [0]*26
-        for s in s1:
-            count1[ord(s)-ord('a')]+=1
-        l = 0
-        r = len(s1)-1
-        for i in range(l,r+1):
-                count2[ord(s2[i])-ord('a')]+=1
-        while r<len(s2):
-            if count1==count2:
+        sorted_s1 = sorted(s1) # O(MlogM), sorting here creates an array from string of size M -> O(M)
+        for i in range(n-m+1): # O(N)
+            if sorted(s2[i:i+m])==sorted_s1: # sorting inside of size M: O(MlogM)
                 return True
-            count2[ord(s2[l])-ord('a')]-=1
-            l+=1
-            r+=1
-            if r<len(s2):
-                count2[ord(s2[r])-ord('a')]+=1
         return False
 
-# Time complexity: O(N), where N is the length of s2. We traverse s2 once and maintain a sliding window of size len(s1).
+sol = Solution()
+ans = sol.permutationStr(s1 = "ab", s2 = "eidbaooo")
+print(ans)
+
+# Time complexity: O(N+M) = O(N), where N is the length of s2. We traverse s2 once and maintain a sliding window of size len(s1).
 # Space complexity: O(1), since we are using a fixed size array of size 26 for the characters a-z.
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        if len(s1)>len(s2):
+        if len(s1)>len(s2): # m = len(s1), n = len(s2)
             return False
         count1 = [0]*26
         count2 = [0]*26
+        
+        # the first m characters (window of m length, index of m-1) of both strings are compared here
         for i in range(len(s1)):
-            count1[ord(s1[i])-ord('a')]+=1
-            count2[ord(s2[i])-ord('a')]+=1 # initialize the first window of size len(s1) in s2
+            count1[ord(s1[i])-ord('a')]+=1 # O(M)
+            count2[ord(s2[i])-ord('a')]+=1 # initialize the first window of size len(s1) in s2, O(N)
         
         if count1==count2:
             return True
 
+        # comparing from index m to n-1
         for i in range(len(s1), len(s2)):
+            # the element beign remove from window = i-m
             count2[ord(s2[i])-ord('a')]+=1
             count2[ord(s2[i-len(s1)])-ord('a')]-=1 # remove the character that is sliding out of the window, here i-len(s1) is the index of the character that is sliding out of the window
             if count1==count2:
@@ -67,7 +66,7 @@ class Solution:
         return False
 
 # Same solution as above but using a dictionary instead of a list for character counts.
-
+# Don't use this, not required, changes nothing in complexity and makes the solution confusing
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
         m, n = len(s1), len(s2)
