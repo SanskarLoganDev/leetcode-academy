de5573d124e611bdf27c8149dd30962a99dcd88c
SanskarLoganDev <devlogan70@gmail.com>
2026-09-01 18:26:34 -0400

    26. Remove Dupliates from an array solution

--- easy/Array_List/remove_duplicates.py
+++ easy/Array_List/remove_duplicates.py
@@ -1,4 +1,5 @@
-# REMOVE DUPLICATES FROM AN ARRAY
+# 26. Remove Dupliates from an array
+# Neetcode 250
 
 # Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
 
@@ -25,6 +26,7 @@
 # Output: 2, nums = [1,2,_]
 # Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
 # It does not matter what you leave beyond the returned k (hence they are underscores).
+
 # Example 2:
 
 # Input: nums = [0,0,1,1,1,2,2,3,3,4]
@@ -33,7 +35,8 @@
 # It does not matter what you leave beyond the returned k (hence they are underscores).
 
 
-
+# Time complexity: O(N^2)
+# Space complexity: O(N)
 # The for loop controls i independently, so modifying it within the loop doesn't impact the loop's progression.
 # This is how I did it in first go:
 def removeDuplicates(nums):
@@ -41,8 +44,8 @@ def removeDuplicates(nums):
         return 0
     res = []
     length = len(nums)
-    for i in range(length):
-        if nums[i] in res:
+    for i in range(length): # O(N)
+        if nums[i] in res: # O(N)
             continue
         else: 
             res.append(nums[i])
@@ -53,19 +56,25 @@ def removeDuplicates(nums):
 
 print(removeDuplicates([1,1,2,2,3,3,3,4,4,4]))
 
-# This is how it should be done using 2 pointers (more efficient)
-def removeDuplicates2(nums):
-    if not nums:
-        return 0  # Handle empty list
-    
-    k = 0  # Initialize the index for the next unique element
-    
-    for i in range(1, len(nums)): # i starts from 1 and k starts from 0
-        if nums[i] != nums[k]: # k only moves forward when repition ends or there is a unique element
-            k += 1
-            nums[k] = nums[i]
-    
-    return k + 1  # Number of unique elements
+from typing import List
 
-print(removeDuplicates([1,1,2,2,3,3,3,4,4,4]))
+# time complexity: O(nlogn)
+# space complexity: O(n) for the set
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        nums[:] = list(set(nums)) # set does not guarantee order
+        nums.sort()
+        return len(nums)
+
+# This is how it should be done using 2 pointers (more efficient)
+# Time complexity: O(n)
+# space complexity: O(1)
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
         
+        for i in range(1, len(nums)): # i starts from 1 and k starts from 0
+            if nums[i] != nums[k]: # k only moves forward when repition ends or there is a unique element
+                k += 1
+                nums[k] = nums[i]
+        
+        return k + 1  # we return k+1 because k starts from 0 and we have to return number of unique elements
